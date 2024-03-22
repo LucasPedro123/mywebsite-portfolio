7a13873a9b8694974c0f7bc8fb247b664939da4c
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-03-22 18:46:49 -0300

    feat: creating the portfolio

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './assets/styles/index.css'
+
+function App() {
+
+  return (
+    <>
+      <section>Hero</section>
+      <section>Parallax</section>
+      <section>Services</section>
+      <section>Parallax</section>
+      <section>Portfolio1</section>
+      <section>Portfolio2</section>
+      <section>Portfolio3</section>
+      <section>Contact</section>
+    </>
+  )
+}
+
+export default App
