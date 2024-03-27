9009deff56012f91d5064ce6130188cdfc264246
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-03-27 01:05:29 -0300

    feat: add links for NavBar

--- src/Component/NavBar/Navbar.jsx
+++ src/Component/NavBar/Navbar.jsx
@@ -9,13 +9,13 @@ function Navbar() {
             <div className="wrapper">
                 <span>Lame Dev</span>
                 <div className="social">
-                    <a href="">
+                    <a href="https://www.instagram.com/lucasfernandes4150/" target='_target'>
                         <i class="fa-brands fa-instagram"></i>
                     </a>
-                    <a href="">
+                    <a href="https://github.com/LucasPedro123" target='_target'>
                         <i class="fa-brands fa-github"></i>
                     </a>
-                    <a href="">
+                    <a href="https://www.linkedin.com/in/lucas-pedro-fernandes/" target='_target'>
                         <i class="fa-brands fa-linkedin"></i>
                     </a>
                 </div>
